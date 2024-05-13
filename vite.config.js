82ae8e891140f895f33a79aefeee7263ee75653f
guignand <t.guignand@adnfamily.com>
2024-05-13 22:25:22 +0200

    feat : update config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import glsl from 'vite-plugin-string';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default defineConfig({
   plugins: [
     react(),
     glsl()
   ],
-  resolve: {
-  },
   css: {
     devSourcemap: true,
   },
