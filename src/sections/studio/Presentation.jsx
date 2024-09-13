b4a3ec32d6eb6bbe117ca2c775b909789c486b08
guignand <t.guignand@adnfamily.com>
2024-09-13 14:33:10 +0200

    feat: distorded effect presentation

--- src/sections/studio/Presentation.jsx
+++ src/sections/studio/Presentation.jsx
@@ -1,5 +1,5 @@
-import React, { useLayoutEffect, useRef } from "react"
-import { gsap } from "gsap"
+import React, {useLayoutEffect, useRef} from "react"
+import {gsap} from "gsap"
 import SplitType from 'split-type'
 import img from "../../img/full1.jpg"
 import image from '../../img/full6.jpg'
@@ -7,6 +7,7 @@ import image2 from '../../img/full9.jpg'
 import image3 from '../../img/full7.jpg'
 import image4 from '../../img/10.jpg'
 import flag from '../../assets/svgs/flag-basque.svg'
+import SketchComponent from "../../components/Sketch.jsx";
 
 const Presentation = () => {
     const titleRefs = useRef([])
@@ -23,7 +24,7 @@ const Presentation = () => {
 
         const splitTextLines = (element) => {
             if (element) {
-                const splitInstance = new SplitType(element, { types: 'lines' })
+                const splitInstance = new SplitType(element, {types: 'lines'})
                 splitInstance.lines.forEach(line => {
                     const wrapper = document.createElement('div')
                     wrapper.style.overflow = 'hidden'
@@ -35,7 +36,7 @@ const Presentation = () => {
 
                 gsap.fromTo(
                     splitInstance.lines,
-                    { opacity: 0, y: 50, rotate: 3 },
+                    {opacity: 0, y: 50, rotate: 3},
                     {
                         opacity: 1,
                         y: 0,
@@ -55,7 +56,7 @@ const Presentation = () => {
             (entries) => {
                 entries.forEach(entry => {
                     if (entry.isIntersecting) {
-                        gsap.to(entry.target, { opacity: 1, y: 0, duration: 0.5 })
+                        gsap.to(entry.target, {opacity: 1, y: 0, duration: 0.5})
                         observer.unobserve(entry.target)
                         const description = entry.target.querySelector('.presentation-section__description')
                         if (description) {
@@ -64,12 +65,12 @@ const Presentation = () => {
                     }
                 })
             },
-            { threshold: 0.1 }
+            {threshold: 0.1}
         )
 
         sectionsRef.current.forEach(section => {
             if (section) {
-                gsap.set(section, { opacity: 0, y: 20 })
+                gsap.set(section, {opacity: 0, y: 20})
                 observer.observe(section)
             }
         })
@@ -79,8 +80,8 @@ const Presentation = () => {
                 const chars = title.querySelectorAll('.char')
                 if (chars.length > 0) {
                     gsap.fromTo(chars,
-                        { opacity: 0, position: 'relative', bottom: -30 },
-                        { opacity: 1, bottom: 0, stagger: 0.05, duration: 0.5, ease: "power2.out", delay: 0.2 }
+                        {opacity: 0, position: 'relative', bottom: -30},
+                        {opacity: 1, bottom: 0, stagger: 0.05, duration: 0.5, ease: "power2.out", delay: 0.2}
                     )
                 }
             }
@@ -107,11 +108,15 @@ const Presentation = () => {
                     <p ref={el => textRefs.current.push(el)} className="presentation-section__description">
                         Notre mission est de créer des œuvres poussées, belles et exceptionnelles.
                         Chaque projet est traité avec le plus grand soin et une attention méticuleuse aux détails.
-                        Notre équipe passionnée et innovante travaille sans relâche pour offrir des solutions uniques et sur mesure.
-                        Nous nous engageons à respecter les délais, à communiquer de manière transparente et à assurer la satisfaction de nos clients à chaque étape.
+                        Notre équipe passionnée et innovante travaille sans relâche pour offrir des solutions uniques et
+                        sur mesure.
+                        Nous nous engageons à respecter les délais, à communiquer de manière transparente et à assurer
+                        la satisfaction de nos clients à chaque étape.
                         Atypica, c'est l'atelier où chaque idée devient une réalité.
                     </p>
-                    <img src={image} alt="Visuel de l'équipe" className="presentation-section__img" />
+                    <div className="presentation-section__box presentation-section__box--first">
+                        <SketchComponent textureImage={image}/>
+                    </div>
                     <div className="flex presentation-section__item-list">
                         <div className="presentation-section__title--secondary">Services</div>
                         <div className="presentation-sectionn__item">Web design</div>
@@ -138,37 +143,48 @@ const Presentation = () => {
                         entreprises pour la croissance économique, mais jamais au détriment de la santé, de la sécurité
                         et de la dignité des individus.
                     </p>
-                    <img src={image2} alt="Visuel de l'équipe" className="presentation-section__img" />
+                    <div className="presentation-section__box presentation-section__box--second">
+                        <SketchComponent textureImage={image2}/>
+                    </div>
                 </div>
             </div>
             <div ref={el => sectionsRef.current.push(el)} className="presentation-section__content--manifest flex">
                 <h2 className="presentation-section__sub-title--manifest">Manifeste</h2>
                 <div>
-                    <p ref={el => textRefs.current.push(el)} className="presentation-section__description presentation-section__description--manifest">
+                    <p ref={el => textRefs.current.push(el)}
+                       className="presentation-section__description presentation-section__description--manifest">
                         Nous croyons en la puissance de la créativité et de l'innovation pour changer le monde.
                         Nous croyons en la collaboration et en l'échange d'idées pour créer des solutions uniques. Nous
-                        croyons en la transparence et en l'honnêteté pour établir des relations durables. Nous croyons en la
-                        passion et en l'engagement pour réaliser des projets exceptionnels. Nous croyons en la qualité et en
+                        croyons en la transparence et en l'honnêteté pour établir des relations durables. Nous croyons
+                        en la
+                        passion et en l'engagement pour réaliser des projets exceptionnels. Nous croyons en la qualité
+                        et en
                         l'excellence pour offrir des résultats exceptionnels. Nous croyons en l'atypique pour créer des
                         expériences mémorables.
                     </p>
-                    <img src={image3} alt="Visuel de l'équipe" className="presentation-section__img"/>
+                    <div className="presentation-section__box presentation-section__box--third">
+                        <SketchComponent textureImage={image3}/>
+                    </div>
                 </div>
             </div>
-            <div ref={el => sectionsRef.current.push(el)} className="presentation-section__content--team flex main-side">
+            <div ref={el => sectionsRef.current.push(el)}
+                 className="presentation-section__content--team flex main-side">
                 <div>Notre équipe</div>
                 <div className="main-side">
-                    <p ref={el => textRefs.current.push(el)} className="presentation-section__description presentation-section__description--team">
+                    <p ref={el => textRefs.current.push(el)}
+                       className="presentation-section__description presentation-section__description--team">
                         Designer et développeur mettent leur expertise au service de votre projet, chaque membre de
                         l'équipe est passionné dans son domaine et s'engage à fournir des solutions innovantes et
                         performantes.
                     </p>
-                    <img src={image4} alt="Visuel de l'équipe" className="presentation-section__img" />
-                </div>
+                    <div className="presentation-section__box presentation-section__box--fourth">
+                        <SketchComponent textureImage={image4}/>
+                    </div>                </div>
             </div>
-            <div ref={el => sectionsRef.current.push(el)} className="presentation-section__content--secondary main-side">
+            <div ref={el => sectionsRef.current.push(el)}
+                 className="presentation-section__content--secondary main-side">
                 <div className="presentation-section__sub-title--secondary flex">Produit à Bayonne
-                    <img src={flag} alt="Drapeau basque" className="flag" />
+                    <img src={flag} alt="Drapeau basque" className="flag"/>
                 </div>
             </div>
         </section>
